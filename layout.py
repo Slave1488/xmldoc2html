3a07f9008a522c595629178b864bee5af2376f00
Slave1488 <blue101blower@gmail.com>
2019-10-24 12:04:54 +0500

    'better_view'

--- layout.py
+++ layout.py
@@ -1,15 +1,19 @@
 from functools import reduce
 from operator import add
+import re
 
 
 def view(content):
     if hasattr(content, 'view'):
         return content.view()
-    return str(content)
+    return f'{content}\n'
+
+
+not_empty_line = re.compile(r'([^\n]+\n)')
 
 
 def move_text(text):
-    return text.replace('\n', '\n\t')
+    return not_empty_line.sub(r'\t\1', text)
 
 
 class Attribute:
@@ -37,8 +41,9 @@ class Tag:
         attrs_view = reduce(
             add, map(lambda attr: f' {view(attr)}', self._attributs), '')
         content_view = reduce(
-            add, map(lambda cont: f'\n{view(cont)}', self._content), '')
-        return '<{name}{attr}>{cont}\n</{name}>'.format(
+            add, map(lambda cont: f'{view(cont)}', self._content), '') or \
+            '\n'
+        return '<{name}{attr}>\n{cont}</{name}>\n'.format(
             name=self._name,
             attr=attrs_view,
             cont=move_text(content_view)
