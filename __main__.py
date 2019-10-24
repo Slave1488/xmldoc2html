7279d917336609fe68c680071dfae12a5a0508f4
Slave1488 <blue101blower@gmail.com>
2019-10-24 12:05:52 +0500

    '~'

--- __main__.py
+++ __main__.py
@@ -4,12 +4,12 @@ from layout import Tag, Attribute, view
 
 with open('content.txt', 'r') as f:
     for part in parserLines.get_simple_parser(f):
-        print(part)
+        print(view(part))
 
 html = Tag('html')
 html.add_attribute(Attribute('name', 'legion'))
 body = Tag('body')
+html.add_content(Tag('head'))
 html.add_content(body)
 body.add_content('text')
-
 print(view(html))
