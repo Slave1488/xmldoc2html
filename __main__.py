3d413086b16eada42212bbd1b5fbbc2846e5e5aa
Slave1488 <blue101blower@gmail.com>
2019-10-30 22:19:44 +0500

    '~'

--- __main__.py
+++ __main__.py
@@ -2,11 +2,11 @@ import reader
 import lineLexer as llexer
 import memberCompiler as mcompiler
 from layout import Tag, Attribute, view
+import mapCreator as mcreator
 
 
 with open('content.txt') as f:
     lr = reader.get_line_reader(f)
     lts = llexer.get_tokens(lr)
     ms = mcompiler.compile(lts)
-    for member in ms:
-        print(view(member))
+    mmap = mcreator.create(*ms)
