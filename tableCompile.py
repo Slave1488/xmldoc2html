353ed6ec90efb32153b3a63246ee772c2662ea9a
Slave1488 <blue101blower@gmail.com>
2019-10-31 12:02:57 +0500

    'add_head_compiler'

--- tableCompile.py
+++ tableCompile.py
@@ -1,7 +1,16 @@
 from layout import Tag
 
 
-def compile_line(content_list):
+def compile_th_tr(name_list):
+    tr = Tag('tr')
+    for cont in name_list:
+        td = Tag('td')
+        td.add_content(*cont)
+        tr.add_content(td)
+    return tr
+
+
+def compile_tr(content_list):
     tr = Tag('tr')
     for cont in content_list:
         td = Tag('td')
@@ -10,13 +19,18 @@ def compile_line(content_list):
     return tr
 
 
-def compile(xmap, xml_tags, caption=None):
+def compile(xmap, xml_tags, *, caption=None, decorate=None):
     table = Tag('table')
     if caption:
         cap_cont = caption
         caption = Tag('caption')
         caption.add_content(cap_cont)
         table.add_content(caption)
+    tr_th = compile_th_tr(xmap.get_names(main='member'))
+    table.add_content(tr_th)
     for xtag in xml_tags:
-        table.add_content(compile_line(xmap.get_content(xtag)))
+        tr = compile_tr(xmap.get_content(xtag))
+        if decorate:
+            tr.add_attrs(*decorate(xtag))
+        table.add_content(tr)
     return table
