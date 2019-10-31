52b48ee8b6f61ff8e1afb737ef8df05e1d458847
Slave1488 <blue101blower@gmail.com>
2019-10-31 12:03:37 +0500

    'add_get_names'

--- tagMap.py
+++ tagMap.py
@@ -4,29 +4,37 @@ import layout
 class Map:
     def __init__(self):
         self._attr_names = set()
-        self._tag_names = {}
+        self._tag_maps = {}
 
-    def __getitem__(self, key):
-        return self._tag_names[key]
+    def __getitem__(self, tag_name):
+        return self._tag_maps[tag_name]
 
-    def __setitem__(self, key, value):
-        self._tag_names[key] = value
+    def __setitem__(self, tag_name, tag_map):
+        self._tag_maps[tag_name] = tag_map
 
     def add_attr_points(self, *attr_names):
         self._attr_names |= set(attr_names)
 
     def add_tag_points(self, *tag_names):
         for name in tag_names:
-            if name not in self._tag_names:
-                self._tag_names[name] = Map()
+            if name not in self._tag_maps:
+                self._tag_maps[name] = Map()
 
     def get_content(self, *tags):
         yield ('\n'.join(tag.get_content()) for tag in tags)
         for aname in self._attr_names:
             yield (attr._value for tag in tags for attr in tag.get_attrs())
-        for tname in self._tag_names:
-            conts = self._tag_names[tname].get_content(
+        for tname in self._tag_maps:
+            conts = self._tag_maps[tname].get_content(
                 *(child_tag for tag in tags
                   for child_tag in tag.get_tags(tname)))
             for cont in conts:
                 yield cont
+
+    def get_names(self, main='main'):
+        yield main
+        for aname in self._attr_names:
+            yield f'{main}.{aname}'
+        for tname in self._tag_maps:
+            for name in self._tag_maps[tname].get_names(main=tname):
+                yield f'{main}.{name}'
