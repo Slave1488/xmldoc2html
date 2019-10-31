9ec0b950fc236970354e617172ecad586a879338
Slave1488 <blue101blower@gmail.com>
2019-10-31 11:58:57 +0500

    '~'

--- __main__.py
+++ __main__.py
@@ -1,15 +1,25 @@
 import reader
 import lineLexer as llexer
 import memberCompiler as mcompiler
-from layout import Tag, Attribute, view
+from layout import Page, view
 import mapCreator as mcreator
 import tableCompile as tcompile
+from tagDecorator import decorate_member, member_stylesheet
 
 
-with open('content.txt') as f, open('html.html', 'w') as html:
-    lr = reader.get_line_reader(f)
-    lts = llexer.get_tokens(lr)
-    ms = list(mcompiler.compile(lts))
-    mmap = mcreator.create(*ms)
-    table = tcompile.compile(mmap, ms, f.name)
-    html.write(view(table))
+def main():
+    with open('content.txt') as f, open('html.html', 'w') as html:
+        lines = reader.get_line_reader(f)
+        lts = llexer.get_tokens(lines)
+        members = mcompiler.compile(lts)
+        member_map = mcreator.create(*members)
+        table = tcompile.compile(
+            member_map, members, caption=f.name,
+            decorate=decorate_member)
+        page = Page()
+        page.add_content(member_stylesheet, table)
+        html.write(page.view())
+
+
+if __name__ == '__main__':
+    main()
